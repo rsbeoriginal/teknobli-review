a8b1ca612b1dd96065c2173849bc5cca0f98a73e
rishi <rishisharma7361@gmail.com>
2019-01-21 11:56:58 +0530

    sendRating to product service

--- review/src/main/java/com/teknobli/review/services/implementation/ProductRatingImpl.java
+++ review/src/main/java/com/teknobli/review/services/implementation/ProductRatingImpl.java
@@ -1,6 +1,7 @@
 package com.teknobli.review.services.implementation;
 
 import com.teknobli.review.dto.ProductRatingDTO;
+
 import com.teknobli.review.entity.ProductRatingEntity;
 import com.teknobli.review.repository.ProductRatingRepository;
 import com.teknobli.review.services.ProductRatingService;
@@ -9,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.client.RestTemplate;
 
 @Service
 @Transactional(readOnly = true,propagation = Propagation.REQUIRES_NEW)
@@ -25,7 +27,18 @@ public class ProductRatingImpl implements ProductRatingService {
         ProductRatingDTO productRatingDTODb = new ProductRatingDTO();
         BeanUtils.copyProperties(productRatingRepository.save(productRatingEntity),productRatingDTODb);
         BeanUtils.copyProperties(productRatingEntity,productRatingDTODb);
+
+        sendRatingToProductService(productRatingDTODb.getProductId(),getRating(productRatingDTODb.getProductId()));
+
         return productRatingDTODb;
+    }
+
+    private void sendRatingToProductService(String productId, Double newRating) {
+        RestTemplate restTemplate = new RestTemplate();
+//        String URL = ProductApi.BASE_URL + EndPoints.createUpdateRatingURL(productId,newRating);
+        String URL = "http://172.16.20.78:8000/product/updaterating/"+productId+"/"+newRating;
+        System.out.println(URL);
+        restTemplate.put(URL, String.class);
     }
 
     @Override
